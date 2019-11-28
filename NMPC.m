c221e093d911831428c7aa77e96a034f502bd292
Lucas Rath <lucasrm25@gmail.com>
2019-11-28 13:52:22 +0100

    Improving MPC performance

---
+++ NMPC.m
@@ -0,0 +1,227 @@
+classdef NMPC
+    %------------------------------------------------------------------
+    % Nonlinear MPC class
+    % 
+    %   solve:
+    %
+    %   MIN     { SUM_{k=0:N-1} fo(tk,xk,uk,ek,r(tk)) } + fend(tN,xN,eN,r(tN))
+    %
+    %   s.t.    xk+1 = f(xk,uk) + Bd*(d(zk)+wk),    wk~N(0,sigmaw^2)
+    %           h(tk,xk,uk,ek) == 0
+    %           g(tk,xk,uk,ek) <= 0
+    %
+    %   for x0,...,xN, u0,...,uN-1, e0,...,eN
+    %
+    %   where xk: state variables
+    %         zk: selected state variables zk=Bd'*xk
+    %         ek: extra variables
+    %         r(tk): trajectory
+    %         tk: current time
+    %------------------------------------------------------------------
+    
+    properties
+        % Optimizer settings
+        maxiter = 200       % max solver iterations
+        tol     = 1e-8      % optimizer tolerance
+        N       = 30        % prediction horizon
+        
+        % Define optimization problem
+        fo      % @fun nonlinear cost function
+        fend    % @fend nonlinear cost function for the final state
+        f       % @fun nonlinear dynamics
+        d @GP   % [GP,GP,...] vector of Gaussian process models representing each disturbance dimension
+        Bd
+        sigmaw  % discrete noise covariance of w
+        h       % equality constraint function 
+        g       % inequality constraint function
+        
+        % Optimization dimension
+        n   % state space dimension
+        m   % input diension
+        ne  % extra variable dimension
+        
+        % time step
+        dt  % time step size
+        
+        isGPactive = false  % if inactive, NMPC will consider d(zk)=0
+    end
+    
+    properties(Access=private)
+        % save last optimal results computed, in order to use as initial guess
+        vars_opt_old = []
+    end
+    
+    methods
+        
+        function obj = NMPC(fo, fend, f, d, Bd, N, sigmaw, h, g, n, m, ne, dt)
+        %------------------------------------------------------------------
+        % MPC constructor
+        % args:
+        %   f0: <1> signal/output stddev
+        %   f:  <1> evaluation noise stddev
+        %   d:  <n,n> length scale covariance matrix
+        %   Bd: <1> maximum dictionary size
+        %------------------------------------------------------------------
+           obj.fo = fo;
+           obj.f  = f;
+           obj.fend = fend;
+           obj.d  = d;
+           obj.Bd = Bd;
+           obj.N = N;
+           obj.sigmaw = sigmaw;
+           obj.h = h;
+           obj.g = g;
+           obj.n = n;
+           obj.m = m;
+           obj.ne = ne;
+           obj.dt = dt;
+        end
+        
+        
+        function u0_opt = optimize(obj, x0, e0, t0, r)
+            z0 = obj.Bd'*x0;
+            
+            if isempty(obj.vars_opt_old)
+                % initialize optimization variables initial guesses
+                xguess = x0*ones(obj.n * obj.N+1,1);     % [ x0,...,xN-1,xN ]
+                uguess = zeros(obj.m * obj.N,1);         % [ u0,...,uN-1    ]
+                eguess = e0*ones(obj.ne * obj.N+1,1);    % [ e0,...,eN-1,eN ]
+                % vector of initial guesses
+                varsguess = [xguess; uguess; eguess];
+            else
+                % use last optimization results as initial guess in case
+                % they have already been calculated once
+                varsguess = obj.vars_opt_old;
+            end
+                
+            % We currently dont need this parameters in fmincon
+            A = [];
+            b = [];
+            Aeq = [];
+            beq = [];
+            lb = [];
+            ub = [];
+            
+            % define cost and constr. function, depending only on the
+            % variables. This is a prerequisite for the function fmincon
+            costfun = @(vars) obj.costfun(vars, t0, r);
+            nonlcon = @(vars) obj.nonlcon(vars, t0, x0);
+            
+            % define optimizer settings
+            options = optimoptions('fmincon',...
+                                    'Display','none',...
+                                   'Algorithm','sqp',...
+                                   'ConstraintTolerance',obj.tol,...
+                                   'MaxIterations',obj.maxiter);
+            
+            % solve optimization problem                   
+            vars_opt = fmincon(costfun,varsguess,A,b,Aeq,beq,lb,ub,nonlcon,options);
+            
+            % store current optimization results to use as initial guess
+            % for future optimizations
+            obj.vars_opt_old = vars_opt;
+            
+            % split variables since vars_opt = [x_opt; u_opt; e_opt]
+            [x_opt, u_opt, e_opt] = splitvariables(obj, vars_opt);
+            u0_opt = u_opt(1:obj.m);
+            
+            % plot results every time step
+            if false
+                figure; hold on; grid on;
+                plot(x_opt, 'DisplayName','x');
+                plot(u_opt, 'DisplayName','u');
+            end
+        end
+        
+    
+        function [xvec, uvec, evec] = splitvariables(obj, vars)
+            % split variables
+            xvec = vars(1:(obj.N+1)*obj.n);
+            uvec = vars( (1:obj.N*obj.m)  + length(xvec) );
+            evec = vars( (1:(obj.N+1)*obj.ne) + length(xvec) + length(uvec) );
+        end
+
+        function cost = costfun(obj, vars, t0, r)
+            % to make the notation shorter
+            n = obj.n;
+            m = obj.m;
+            ne = obj.ne;
+            N = obj.N;
+
+            % split variables
+            [xvec, uvec, evec] = obj.splitvariables(vars);
+
+            cost = 0;
+            t = t0;
+            for iN=1:N+1      % i=0:N
+                
+                % which are the indices of the current time k?
+                idx_xk =  n*(iN-1)+1 : n*iN;
+                idx_uk =  m*(iN-1)+1 : m*iN;
+                idx_ek = ne*(iN-1)+1 : ne*iN;
+
+                % calculate cost for t = tk
+                if iN <= N
+                    % cost for current state
+                    cost = cost + obj.fo( t, xvec(idx_xk), uvec(idx_uk), evec(idx_ek), r );
+                else
+                    % final state cost
+                    cost = cost + obj.fend( t, xvec(idx_xk), evec(idx_ek), r );
+                end
+                
+                % update current time
+                t = t + iN * obj.dt;
+            end
+            
+        end
+
+        function [cineq,ceq] = nonlcon(obj,vars, t0, x0)
+
+            % to make the notation shorter
+            n = obj.n;
+            m = obj.m;
+            ne = obj.ne;
+            N = obj.N;
+
+            % split variables
+            [xvec, uvec, evec] = obj.splitvariables(vars);
+            
+            % initial state constraint: x0 - x(0) = 0
+            ceq = [ x0 - xvec(1) ];
+            cineq = [];
+            
+            t = t0;
+            for iN=1:N
+
+                % which are the indices of the current time k?
+                idx_xk   =  n*(iN-1)+1 : n*iN;
+                idx_uk   =  m*(iN-1)+1 : m*iN;
+                idx_ek   = ne*(iN-1)+1 : ne*iN;
+
+                
+                if obj.isGPactive
+                    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                    % TODO:
+                    %       calculate constraint when GP is active
+                    %       see Formula Student AMZ paper
+                    %       mean and covariance needs to be propagated with
+                    %       EKF
+                    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                else 
+                    % add dynamics constraints without GP disturbance estimation
+                    ceq = [ceq ;
+                           xvec(idx_xk+n) - obj.f(t,xvec(idx_xk),uvec(idx_uk)) ;
+                           obj.h(t,xvec(idx_xk),uvec(idx_uk),evec(idx_ek))     ];
+                    
+                    cineq = [cineq;
+                             obj.g(t,xvec(idx_xk),uvec(idx_uk),evec(idx_ek)) ];
+                end
+                t = t + iN * obj.dt;
+            end
+
+        end
+        
+    end
+end
+
+
--- ex_toyexample.m
+++ ex_toyexample.m
@@ -8,7 +8,7 @@
 
 clear vars; close all; clc;
 
-dt = 0.01;
+dt = 0.05;
 
 %% Dynamic Model
 %------------------------------------------------------------------
@@ -40,29 +40,6 @@ f_true = @(x,u) A*x + B*u + Bd*(d_true(x,u));
 fd_true = @(x,u,dt,inclnoise) x + dt*f_true(x,u) + inclnoise*sqrt(dt)*Bd*sigmaw*randn(md);
 
 
-%% Controller
-
-% nominal model
-f = @(x,u) A*x + B*u;
-% discretize nominal model
-fd = @(x,u,dt) x + dt*f(x,u);
-
-% LQR controller
-Ak = eye(n)+dt*A;
-Bk = B*dt;
-K = place(Ak,Bk,0.9);
-
-% Prefilter
-Kr = inv((eye(n)-Ak+Bk*K)\Bk);
-
-eig(Ak-Bk*K)
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% TODO:
-%       - Implement MPC controller - See MPC.m class
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 
 %% Gaussian Process
 
@@ -70,10 +47,57 @@ eig(Ak-Bk*K)
 sigmaf  = 0.1;              % output variance (std)
 lambda  = diag([2,10].^2);   % length scale
 sigman  = sigmaw*sqrt(dt);  % stddev of measurement noise
-maxsize = 100;              % maximum number of points in the dictionary
+maxsize = 200;              % maximum number of points in the dictionary
 
 % create GP object
 gp = GP(sigmaf, sigman, lambda, maxsize);
+
+
+
+%% Controller
+
+% -------------------------------------------------------------------------
+% DEFINE NOMINAL MODEL
+% nominal continuous time model
+f = @(x,u) A*x + B*u;
+% discretize nominal model
+fd = @(x,u,dt) x + dt*f(x,u);
+% -------------------------------------------------------------------------
+
+
+
+% -------------------------------------------------------------------------
+% LQR CONTROLLER
+Ak = eye(n)+dt*A;
+Bk = B*dt;
+K = place(Ak,Bk,0.9);
+% Prefilter
+Kr = inv((eye(n)-Ak+Bk*K)\Bk);
+% check eigenvalues
+eig(Ak-Bk*K);
+% -------------------------------------------------------------------------
+
+
+% -------------------------------------------------------------------------
+% NONLINEAR MPC CONTROLLER
+% define cost function
+N = 20;     % prediction horizon
+Q = 100;
+R = 1;
+f    = @(t,x,u) fd(x,u,dt);
+fo   = @(t,x,u,e,r)  1*(x-r(t))'*Q*(x-r(t)) + R*u^2;
+fend = @(t,x,e,r)   10*(x-r(t))'*Q*(x-r(t));
+h    = @(t,x,u,e) 0;
+g    = @(t,x,u,e) 0;
+
+mpc = NMPC(fo, fend, f, gp, Bd, N, sigmaw, h, g, n, m, 0, dt);
+
+x0 = 10;
+e0 = 0;
+t0 = 0;
+r  = @(t)2;    % desired trajectory
+u0 = mpc.optimize(x0, e0, t0, r );
+% -------------------------------------------------------------------------
 
 
 %% Simulate
@@ -85,7 +109,7 @@ r = @(t) 5*sin(5*t) + 5*sin(15*t) + 10*exp(-t);
 nr = 1; % dimension of r(t)
 
 % simulation time
-tf = 10;
+tf = 5;
 
 % initial state
 x0 = 0;
@@ -106,7 +130,11 @@ for i = 1:numel(out.t)-1
     out.r(:,i) = r(out.t(i));
     
     % calculate control input
-    out.u(:,i) = Kr*out.r(:,i) - K*out.xhat(:,i);
+%     out.u(:,i) = Kr*out.r(:,i) - K*out.xhat(:,i);
+    x0 = out.xhat(:,i);
+    e0 = 0;
+    t0 = out.t(i);
+    out.u(:,i) = mpc.optimize(x0, e0, t0, r);
     
     % simulate real model
     out.x(:,i+1) = fd_true(out.x(:,i),out.u(:,i),dt,true);
@@ -116,7 +144,7 @@ for i = 1:numel(out.t)-1
     
     % add data to GP model
     out.xnom(:,i+1) = fd(out.xhat(:,i),out.u(:,i),dt);
-    if mod(i-1,7)==0
+    if mod(i-1,3)==0
         ddata = Bd \ (out.xhat(:,i+1) - out.xnom(:,i+1));
         gp.add( [out.xnom(:,i);out.u(:,i)], ddata );
     end
@@ -139,9 +167,13 @@ gptrue = @(x) Bd \ ( fd_true(x(1),x(2),dt,false) - fd(x(1),x(2),dt) );
 gp.plot2d( gptrue )
        
 % plot reference and state signal
-figure; hold on; grid on;
+figure; 
+subplot(2,1,1); hold on; grid on;
 plot(out.t(1:end-1), out.r, 'DisplayName', 'r(t)')
 plot(out.t, out.x, 'DisplayName', 'x(t)')
+legend;
+subplot(2,1,2); hold on; grid on;
+plot(out.t(1:end-1), out.u, 'DisplayName', 'u(t)')
 legend;
 
 
